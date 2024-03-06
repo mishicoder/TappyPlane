15bc9140a2bde46b7155c1951f8f9b5a4103f03b
Mishicoder <56014494+mishicoder@users.noreply.github.com>
2024-03-05 19:52:06 -0500

    update

---
+++ src/utils/rectangle_transition.js
@@ -0,0 +1,22 @@
+
+function rectangleTransition(){
+
+    return {
+
+        id: 'rect_transition',
+        require: [],
+
+        add(){},
+        update(){},
+        draw(){},
+        destroy(){},
+        inspect(){},
+
+        // all me
+
+
+    };
+
+}
+
+export default rectangleTransition;
